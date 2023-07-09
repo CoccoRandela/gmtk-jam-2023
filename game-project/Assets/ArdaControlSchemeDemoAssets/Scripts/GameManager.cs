6796cc1c0ace8cb684eb2709835eb4213c940263
CoccoRandela <rocco.candela94@gmail.com>
2023-07-09 18:55:26 +0200

    last

--- game-project/Assets/ArdaControlSchemeDemoAssets/Scripts/GameManager.cs
+++ game-project/Assets/ArdaControlSchemeDemoAssets/Scripts/GameManager.cs
@@ -86,6 +86,8 @@ public class GameManager : MonoBehaviour
         {
             GameObject.Destroy(mole.gameObject);
         }
+
+        molesInGame.Clear();
         StartCoroutine("TakeAwayHoles");
     }
 
